58ade65e3d0b72d501abbdb4977489c9f6848f2a
larsLotNav <lars.august.lotsberg@nav.no>
2022-09-26 12:52:14 +0200

    Logger header

--- src/pages/api/schedules.ts
+++ src/pages/api/schedules.ts
@@ -6,6 +6,7 @@ export default async function handler(req : NextApiRequest, res: NextApiResponse
   
 
   let authorizationHeader = req.headers && req.headers.authorization?  req.headers.authorization: "No Authorization header"
+  console.log(authorizationHeader)
 
   let path = "https://vaktor-plan-api.dev.intern.nav.no/api/v1/schedules/"
   const backendResponse = await fetch(
