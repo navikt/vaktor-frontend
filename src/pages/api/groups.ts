6120e4c2d19a01efec11fe8dc72e7af44414301f
larsLotNav <lars.august.lotsberg@nav.no>
2022-09-26 12:47:29 +0200

    Added groups api

---
+++ src/pages/api/groups.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+
+export default async function handler(req : NextApiRequest, res: NextApiResponse) {
+  
+
+  let authorizationHeader = req.headers && req.headers.authorization?  req.headers.authorization: "No Authorization header"
+
+  let path = "https://vaktor-plan-api.dev.intern.nav.no/api/v1/groups/"
+  const backendResponse = await fetch(
+      path,
+      {
+          headers: {'Authorization': authorizationHeader},
+      },
+    )
+
+  await backendResponse.json()
+  .then(body => {
+      if (body) {
+          res.status(200).json(body)
+      }
+      else {
+          res.send("Cant get data from backend")
+      }
+
+  })
+}
