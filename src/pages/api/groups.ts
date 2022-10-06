08a5f0c9425903ec9a283fde86d52b1ffb8e284f
larsLotNav <lars.august.lotsberg@nav.no>
2022-10-06 10:08:30 +0200

    Backend prod

--- src/pages/api/groups.ts
+++ src/pages/api/groups.ts
@@ -8,7 +8,7 @@ export default async function handler(req : NextApiRequest, res: NextApiResponse
 
   let authorizationHeader = req.headers && req.headers.authorization?  req.headers.authorization: "No Authorization header"
 
-  let path = "https://vaktor-plan-api.dev.intern.nav.no/api/v1/groups/"
+  let path = "https://vaktor-plan-api.intern.nav.no/api/v1/groups/"
   const backendResponse = await fetch(
       path,
       {
--- src/pages/api/schedules.ts
+++ src/pages/api/schedules.ts
@@ -7,7 +7,7 @@ export default async function handler(req : NextApiRequest, res: NextApiResponse
 
   let authorizationHeader = req.headers && req.headers.authorization?  req.headers.authorization: "No Authorization header"
 
-  let path = "https://vaktor-plan-api.dev.intern.nav.no/api/v1/schedules/"
+  let path = "https://vaktor-plan-api.intern.nav.no/api/v1/schedules/"
   const backendResponse = await fetch(
       path,
       {
