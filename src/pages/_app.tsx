68bc131c46b1ea2e76c96b42b3c3f4a0e3b0e222
Christer Gabrielsen <christer.gabrielsen@nav.no>
2022-10-17 14:47:23 +0200

    Added navbar and some styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import "../styles/tidslinje.scss";
 import "../styles/globals.css";
+import "../styles/ApproveSchema.css";
+
 import "@navikt/ds-css";
 import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp;
