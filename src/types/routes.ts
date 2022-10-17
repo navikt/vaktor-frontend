1df48d56ec2c9866fe2d2c31bf1cb0b729fc3fc0
Christer Gabrielsen <christer.gabrielsen@nav.no>
2022-10-17 14:35:36 +0200

    Added routes

---
+++ src/types/routes.ts
@@ -0,0 +1,26 @@
+// Landing page
+export enum RouterVaktor {
+    NAME = "Vaktor",
+    PATH = "/"
+}
+// ---
+
+// Admin
+export enum RouterAdmin {
+    NAME = "Admin - Ansattgodkjenning",
+    PATH = "/admin"
+}
+
+export enum RouterLedergodkjenning {
+    NAME = "Admin - Ledergodkjenning",
+    PATH = "/adminleder"
+}
+// ---
+
+
+// Error
+export enum RouterError {
+    NAME = "Error side",
+    PATH = "/Custom404"
+}
+// ---
