61d68f0835ce00a302f3901951fc18830537e181
Christer Gabrielsen <christer.gabrielsen@nav.no>
2022-12-07 12:57:15 +0100

    Feat: Adds Vaktor santa favicons for December

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,16 @@ import { User } from "../types/types"
 import { Header, Footer } from "@navikt/status-components-react"
 import Head from "next/head"
 
+
+const today = new Date()
+// let icon_url = ""
+
+// today.getMonth() === 11
+// ? icon_url = "santa-facicon"
+// : icon_url = "images"
+
+
+
 const Layout = ({ children }: any) => {
     const [userData, setUserData] = useState<User>({} as User)
     useEffect(() => {
@@ -19,27 +29,54 @@ const Layout = ({ children }: any) => {
     return (
         <div className="mainContainer">
             <Header imageURL="/vaktor/assets/navblack.png" />
-            <Head>
-                <title>Vaktor - Beredskapsvakt</title>
-                <link rel="shortcut icon" href="/vaktor/images/favicon.ico" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/vaktor/images/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/vaktor/images/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/vaktor/images/favicon-16x16.png"
-                />
-            </Head>
+
+            {today.getMonth() === 11
+                ?
+                <Head>
+                    <title>Vaktor - Beredskapsvakt</title>
+                    <link rel="shortcut icon" href="/vaktor/santa-favicon/favicon.ico" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/vaktor/santa-favicon/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/vaktor/santa-favicon/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/vaktor/santa-favicon/favicon-16x16.png"
+                    />
+                </Head>
+                :
+                <Head>
+                    <title>Vaktor - Beredskapsvakt</title>
+                    <link rel="shortcut icon" href="/vaktor/images/favicon.ico" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/vaktor/images/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/vaktor/images/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/vaktor/images/favicon-16x16.png"
+                    />
+                </Head>
+            }
+
             <div className="content">
                 <div className="topHeader"></div>
                 <Navbar />
