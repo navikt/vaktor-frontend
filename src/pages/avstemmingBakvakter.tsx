5598b424110e4c58a156f2ae4357896d5ae9e141
Jonas Juvet <jonas.juvet@nav.no>
2023-01-10 14:56:15 +0100

    feat: can now view a bakvakt report

---
+++ src/pages/avstemmingBakvakter.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from "next"
+import moment from "moment"
+import "moment/locale/nb"
+import AvstemmingBakvakter from "../components/AvstemmingBakvakter"
+import { GuidePanel } from "@navikt/ds-react"
+import { User } from "../types/types"
+import { useEffect, useState } from "react"
+
+const Home: NextPage = () => {
+    const [userData, setUserData] = useState<User>({} as User)
+
+    moment.locale("nb")
+    useEffect(() => {
+        Promise.all([fetch("/vaktor/api/get_me")])
+            .then(async ([current_user]) => {
+                const userjson = await current_user.json()
+                return [userjson]
+            })
+            .then(([userData]) => {
+                setUserData(userData)
+            })
+    }, [])
+
+    if (["okonomi", "leveranseleder"].includes(userData.role))
+        return (
+            <>
+                <div className="Container">
+                    <div className="AdminGuideContainer">
+                        <GuidePanel className="AdminGuidePanel">
+                            <p>Avstemming for ØT </p>
+                        </GuidePanel>
+                    </div>
+                    <AvstemmingBakvakter />
+                </div>
+            </>
+        )
+    return <div>Du har ikke tilgang hit!</div>
+}
+
+export default Home
