b1003c654516001f1661b88a6ed0f1cc967d0f47
Jonas Juvet <jonas.juvet@nav.no>
2023-04-28 14:32:07 +0200

    test: test connection to fullmaktsapi

--- src/pages/api/fullmakt_test.ts
+++ src/pages/api/fullmakt_test.ts
@@ -3,8 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // for prod / dev
     let authorizationHeader = req.headers && req.headers.authorization ? req.headers.authorization : 'No Authorization header'
-
-    let path = `${process.env.BACKEND_URL}/api/v1/admin/get_fullmakter`
+    console.log(authorizationHeader)
+    let path = `${process.env.BACKEND_URL}/api/v1/admin/test_fullmakt`
+    //let path = `https://fullmaktsregister-api.intern.dev.nav.no/api/v1/fullmakter`
 
     const backendResponse = await fetch(path, {
         headers: { Authorization: authorizationHeader },
