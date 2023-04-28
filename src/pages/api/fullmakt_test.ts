d8dc50947946fb1a794e6055e233bfba49b8d38d
Jonas Juvet <jonas.juvet@nav.no>
2023-04-28 15:05:09 +0200

    test: log token

--- src/pages/api/fullmakt_test.ts
+++ src/pages/api/fullmakt_test.ts
@@ -4,14 +4,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // for prod / dev
     let authorizationHeader = req.headers && req.headers.authorization ? req.headers.authorization : 'No Authorization header'
     console.log(authorizationHeader)
-    let path = `${process.env.BACKEND_URL}/api/v1/admin/test_fullmakt`
-    //let path = `https://fullmaktsregister-api.intern.dev.nav.no/api/v1/fullmakter`
+    //let path = `${process.env.BACKEND_URL}/api/v1/admin/test_fullmakt`
+    let path = `https://fullmaktsregister-external-proxy.intern.dev.nav.no/api/v1/fullmakter`
 
     const backendResponse = await fetch(path, {
         headers: { Authorization: authorizationHeader },
     })
 
     console.log(backendResponse)
+
     await backendResponse.json().then((body) => {
         if (body) {
             console.log('body: ', body)
