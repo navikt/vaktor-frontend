87d19f3c4c92e58f4fc44e7ef816d9d58dfd259c
christergabba <christergabba@gmail.com>
2023-05-09 12:25:17 +0200

    Fix faketoken and revert changes to nais.yml

--- src/pages/api/confirm_schedule.ts
+++ src/pages/api/confirm_schedule.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    //let authorizationHeader = req.headers && req.headers.authorization ? req.headers.authorization : "No Authorization header"
-    let authorizationHeader = process.env.FAKE_TOKEN
+    let authorizationHeader = req.headers && req.headers.authorization ? req.headers.authorization : 'No Authorization header'
+    //let authorizationHeader = process.env.FAKE_TOKEN
 
     try {
         let schedule_id = req.query.schedule_id
@@ -12,7 +12,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
 
         //let path = `${process.env.BACKEND_URL}/api/v1/schedules/${schedule_id}/confirm`
-        let path = `http://vaktor-plan-api/api/v1/schedules/${schedule_id}/confirm`
+        //vaktor-plan-api.intern.dev.nav.no/
+        let path = `http://vaktor-plan/api/v1/schedules/${schedule_id}/confirm`
 
         const fetchOptions = {
             headers: { Authorization: authorizationHeader },
