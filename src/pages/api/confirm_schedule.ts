36bf0eeb02c9100dcee908c3ab9422b6cc0727d3
christergabba <christergabba@gmail.com>
2023-05-09 11:48:33 +0200

    Fix: Code scanning alerts #3

--- src/pages/api/confirm_schedule.ts
+++ src/pages/api/confirm_schedule.ts
@@ -1,24 +1,35 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // for prod / dev
-    let authorizationHeader = req.headers && req.headers.authorization ? req.headers.authorization : 'No Authorization header'
     //let authorizationHeader = req.headers && req.headers.authorization ? req.headers.authorization : "No Authorization header"
-    // for local testing
+    let authorizationHeader = process.env.FAKE_TOKEN
 
-    let schedule_id = req.query.schedule_id
-    let path = `${process.env.BACKEND_URL}/api/v1/schedules/${schedule_id}/confirm`
-
-    const backendResponse = await fetch(path, {
-        headers: { Authorization: authorizationHeader },
-        method: 'POST',
-    })
-
-    await backendResponse.json().then((body) => {
-        if (body) {
-            res.status(200).json(body)
-        } else {
-            res.send('Cant get data from backend')
+    try {
+        let schedule_id = req.query.schedule_id
+        // validate schedule_id parameter
+        if (!schedule_id || schedule_id === '') {
+            throw new Error('Invalid schedule ID')
         }
-    })
+
+        //let path = `${process.env.BACKEND_URL}/api/v1/schedules/${schedule_id}/confirm`
+        let path = `http://vaktor-plan/api/v1/schedules/${schedule_id}/confirm`
+
+        const fetchOptions = {
+            headers: { Authorization: authorizationHeader },
+            method: 'POST',
+        }
+
+        const backendResponse = await fetch(path, fetchOptions)
+
+        if (backendResponse.ok) {
+            const responseData = await backendResponse.json()
+            res.status(200).json(responseData)
+        } else {
+            const errorMessage = await backendResponse.json()
+            res.status(backendResponse.status).json({ message: errorMessage.message || 'Unable to confirm schedule' })
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Error occurred while processing your request.' })
+    }
 }
