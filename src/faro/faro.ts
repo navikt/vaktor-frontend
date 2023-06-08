ed8e3a78fcbdbb58cbaa6e15732eecd5e53af9b9
christergabba <christergabba@gmail.com>
2023-06-08 14:56:49 +0200

    debug

--- src/faro/faro.ts
+++ src/faro/faro.ts
@@ -4,14 +4,16 @@ import { Faro, getWebInstrumentations, initializeFaro, LogLevel } from '@grafana
 let faro: Faro | null = null
 export function initInstrumentation(): void {
     if (typeof window === 'undefined' || faro !== null) return
-
     getFaro()
 }
+
+const url = process.env.NEXT_PUBLIC_TELEMETRY_URL
+console.log("This is the faro-url being used:",url)
 
 export function getFaro(): Faro {
     if (faro != null) return faro
     faro = initializeFaro({
-        url: process.env.NEXT_PUBLIC_TELEMETRY_URL,
+        url: url,
         app: {
             name: 'vaktor',
         },
