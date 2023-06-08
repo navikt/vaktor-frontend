dddd7e624d06e8da4c1d7cbdf30cefa1732ac020
christergabba <christergabba@gmail.com>
2023-06-08 16:26:35 +0200

    More testing this env thing

--- src/faro/faro.ts
+++ src/faro/faro.ts
@@ -1,36 +1,20 @@
 import { Faro, getWebInstrumentations, initializeFaro, LogLevel } from '@grafana/faro-web-sdk'
+import nais from '../../nais.js';
+
+// import { TracingInstrumentation } from '@grafana/faro-web-tracing'
 
 let faro: Faro | null = null
+export function initInstrumentation(): void {
+    if (typeof window === 'undefined' || faro !== null) return
 
-export async function initInstrumentation(): Promise<void> {
-  if (typeof window === 'undefined' || faro !== null) return
-
-  const url = await fetch('/vaktor/api/env')
-    .then((response) => response.text())
-    .catch((error) => {
-      console.error('Failed to fetch the faro-url:', error)
-      return ''
-    })
-
-  console.log('This is the faro-url being used:', url)
-  if (url) {
-    faro = initializeFaro({
-      url,
-      app: {
-        name: 'vaktor',
-      },
-      instrumentations: [
-        ...getWebInstrumentations({
-          captureConsole: false,
-        }),
-        // new TracingInstrumentation(),
-      ],
-    })
-  }
+    getFaro()
 }
 
 export function getFaro(): Faro {
-  if (faro !== null) return faro
-
-  throw new Error('Faro has not been initialized.')
+    if (faro != null) return faro
+    faro = initializeFaro({
+      url: nais.telemetryCollectorURL,
+      app: nais.app
+    })
+    return faro
 }
--- src/pages/api/env.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const url = process.env.NEXT_PUBLIC_TELEMETRY_URL || ''
-  res.send(url)
-}
