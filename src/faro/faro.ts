77f55fc5d1464adaacb0d9679ad4f8f6a629749e
christergabba <christergabba@gmail.com>
2023-06-22 14:11:54 +0200

    src/faro/faro.ts

--- src/faro/faro.ts
+++ src/faro/faro.ts
@@ -1,8 +1,7 @@
-import { Faro, getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
-import { TracingInstrumentation } from '@grafana/faro-web-tracing'
+import { Faro, getWebInstrumentations, initializeFaro, LogLevel } from '@grafana/faro-web-sdk'
+//import { TracingInstrumentation } from '@grafana/faro-web-tracing'
 
 let faro: Faro | null = null
-
 export function initInstrumentation(): void {
     if (typeof window === 'undefined' || faro !== null) return
 
@@ -11,7 +10,7 @@ export function initInstrumentation(): void {
 
 export function getFaro(): Faro {
     if (faro != null) return faro
-
+    console.log('The faro url:', process.env.NEXT_PUBLIC_TELEMETRY_URL)
     faro = initializeFaro({
         url: process.env.NEXT_PUBLIC_TELEMETRY_URL,
         app: {
@@ -21,9 +20,8 @@ export function getFaro(): Faro {
             ...getWebInstrumentations({
                 captureConsole: false,
             }),
-            new TracingInstrumentation(),
+            // new TracingInstrumentation(),
         ],
     })
-
     return faro
 }
