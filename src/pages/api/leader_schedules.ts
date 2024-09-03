aa69878b2356003f7973c3ee590613b393e26cb9
christergabba <christergabba@gmail.com>
2024-09-03 14:29:46 +0200

    Fix: increase performance by fetching leader schedules with timestamp limits

--- src/pages/api/leader_schedules.ts
+++ src/pages/api/leader_schedules.ts
@@ -7,7 +7,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         authorizationHeader = process.env.FAKE_TOKEN
     }
 
-    let path = `${process.env.BACKEND_URL}/api/v1/leaders/users/schedules`
+    const start_timestamp = encodeURIComponent(req.query.start_timestamp as string)
+    const end_timestamp = encodeURIComponent(req.query.end_timestamp as string)
+
+    if (!start_timestamp && !end_timestamp) {
+        return res.status(400).json({ message: 'Missing required parameter start and/or end timestamps' })
+    }
+
+    // const path = `${process.env.BACKEND_URL}/api/v1/okonomi/all_schedules_with_limit?start=${start_timestamp}&end=${end_timestamp}`
+
+    //let path = `${process.env.BACKEND_URL}/api/v1/leaders/users/schedules`
+    let path = `${process.env.BACKEND_URL}/api/v1/leaders/users/schedules_with_limit?start_timestamp=${start_timestamp}&end_timestamp=${end_timestamp}`
 
     const backendResponse = await fetch(path, {
         headers: { Authorization: authorizationHeader },
