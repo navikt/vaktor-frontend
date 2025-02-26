c6f768bf9804ecf8ea41aadc494922934153bb65
christergabba <christergabba@gmail.com>
2025-02-26 10:34:41 +0100

    Fix: corrected date format for bakvakter

--- src/components/ScheduleChanges.tsx
+++ src/components/ScheduleChanges.tsx
@@ -9,9 +9,23 @@ const ScheduleChanges = (props: { periods: Schedules[]; setResponse: Dispatch<an
                 <div key={index}>
                     <b>{bakvakter.user.name}</b>
                     <br />
-                    Fra: {new Date(bakvakter.start_timestamp * 1000).toLocaleString().slice(0, -3)}
+                    Fra:{' '}
+                    {new Date(bakvakter.start_timestamp * 1000).toLocaleString('no-NB', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    })}
                     <br />
-                    Til: {new Date(bakvakter.end_timestamp * 1000).toLocaleString().slice(0, -3)}
+                    Til:{' '}
+                    {new Date(bakvakter.end_timestamp * 1000).toLocaleString('no-NB', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    })}
                     <br />
                     {bakvakter.approve_level === 0 ? (
                         <>
